2c24284e80989281646311cf4697ae19031c66c7
breadbyte <14045257+breadbyte@users.noreply.github.com>
2021-04-08 03:17:50 +0800

    add patch and unpatch function, make patch definition inline

--- patch1337tocpp/Program.cs
+++ patch1337tocpp/Program.cs
@@ -11,7 +11,7 @@ namespace patch1337tocpp {
     class Program {
         static void Main(string[] args) {
             CommandLineOpts opts = null;
-            
+
             Parser.Default.ParseArguments<CommandLineOpts>(args)
                   .WithParsed<CommandLineOpts>(o => opts = o);
 
@@ -21,7 +21,7 @@ namespace patch1337tocpp {
             StringBuilder finalOutput = new StringBuilder();
 
             Patch? currentPatch = null;
-            
+
             foreach (var line in lines) {
                 if (line.StartsWith('>')) {
                     var p = new Patch() {ModuleName = line.Substring(1, line.Length - 1)};
@@ -32,7 +32,7 @@ namespace patch1337tocpp {
                 else {
                     if (!currentPatch.HasValue)
                         throw new InvalidDataException("Patch does not start with a valid module identifier!");
-                    
+
                     currentPatch.Value.Patches.Add(new PatchAddress() {
                         Address = int.Parse(line.Substring(0, 8), NumberStyles.HexNumber),
                         OldByte = byte.Parse(line.Substring(9, 2), NumberStyles.HexNumber),
@@ -41,9 +41,11 @@ namespace patch1337tocpp {
                 }
             }
 
+
             using (StreamWriter w = new StreamWriter("output.cpp")) {
-                
+
                 w.WriteLine("#include <vector>");
+                w.WriteLine("#include <windows.h>");
                 w.Write("struct PatchAddress { ");
                 w.Write("int Address; ");
                 w.Write("unsigned char OldByte; ");
@@ -53,20 +55,34 @@ namespace patch1337tocpp {
                 w.Write("char* ModuleName; ");
                 w.Write("std::vector<PatchAddress> Patches;");
                 w.WriteLine("};");
+                w.WriteLine("void PatchUChar(unsigned char* dst, unsigned char* src, int size) {");
+                w.WriteLine("DWORD oldprotect;");
+                w.WriteLine("VirtualProtect(dst, size, PAGE_EXECUTE_READWRITE, &oldprotect);");
+                w.WriteLine("memcpy(dst, src, size);");
+                w.WriteLine("VirtualProtect(dst, size, oldprotect, &oldprotect); };");
 
                 foreach (var patch in patches) {
                     var addressableName = Regex.Replace(patch.ModuleName, "([^A-Za-z0-9])+", "_");
-                    w.WriteLine($"Patch {addressableName};");
-                    w.WriteLine($"{addressableName}.ModuleName = \"{patch.ModuleName}\";");
-                    w.WriteLine($"{addressableName}.Patches = {{");
 
-                    foreach (var addr in patch.Patches) { 
+                    w.WriteLine($"Patch {addressableName} = Patch {{ \"{patch.ModuleName}\", {{");
+
+                    foreach (var addr in patch.Patches) {
                         w.WriteLine($"PatchAddress{{ {addr.Address}, {addr.OldByte}, {addr.NewByte} }},");
                     }
 
+                    w.WriteLine("} };");
+
+                    w.WriteLine($"void patch_{addressableName}() {{");
+                    w.WriteLine($"for (PatchAddress addr : {addressableName}.Patches) {{");
+                    w.WriteLine("PatchUChar((unsigned char*)addr.Address, (unsigned char*)addr.NewByte, 1); }");
+                    w.WriteLine("};");
+                    
+                    w.WriteLine($"void unpatch_{addressableName}() {{");
+                    w.WriteLine($"for (PatchAddress addr : {addressableName}.Patches) {{");
+                    w.WriteLine("PatchUChar((unsigned char*)addr.Address,(unsigned char*)addr.OldByte, 1); }");
                     w.WriteLine("};");
                 }
-
+                
                 w.Flush();
             }
         }
